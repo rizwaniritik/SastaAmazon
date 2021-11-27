5af8518af019511fe919af04b0192fb6d7d325d7
rizwaniritik <rizwaniritik@gmail.com>
2021-11-27 20:58:32 +0530

    Upload

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import environ
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -126,3 +126,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = "/image/download/"
 MEDIA_ROOT = BASE_DIR
+django_heroku.settings(locals())
