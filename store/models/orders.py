4d52370dd851353092a4ef32b941bc6bfaef41ef
rizwaniritik <rizwaniritik@gmail.com>
2021-11-27 20:50:01 +0530

    Upload

---
+++ store/models/orders.py
@@ -0,0 +1,29 @@
+from django.db import models
+from .product import Product
+from .customer import Customer
+import datetime
+
+
+class Order(models.Model):
+    now=datetime.datetime.now()
+    noww=now.strftime('%Y%M%H%M%S')
+    ans='OD'+noww
+    product = models.ForeignKey(Product,
+                                on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer,
+                                 on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    price = models.IntegerField()
+    address = models.CharField(max_length=50, default='', blank=True)
+    phone = models.CharField(max_length=50, default='', blank=True)
+    date = models.DateField(default=datetime.datetime.today)
+    orderid=models.CharField(max_length=50, default=ans, blank=True)
+    status = models.BooleanField(default=False)
+
+    def placeOrder(self):
+        self.save()
+
+    @staticmethod
+    def get_orders_by_customer(customer_id):
+        return Order.objects.filter(customer=customer_id).order_by('-date')
+
